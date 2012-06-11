2b23bf6a56d3f20c5ca6034d6009a900c73a5e8e
carlos <huaixian.huang@gmail.com>
2012-06-11 18:13:36 +0800

    get time section ok

--- git_analyse.py
+++ git_analyse.py
@@ -6,10 +6,11 @@ import string
 import commands
 
 time_pos = 1
+time_of_hour = [0,0,0,0,0,0,0,0]
+time_step = 3
 
 def run_sys_command(command):
   status,output_file = commands.getstatusoutput(command.strip())
-  print "\"%s\" return: "%(command.strip()),"status: ", status
   if(status != 0):
     print command.strip(),"return error!!! we only do to is exit"
     sys.exit()
@@ -25,10 +26,22 @@ else:
   who = sys.argv[1]
   work_path = sys.argv[2]
   os.chdir(work_path)
-  get_git_command = "git log --author=\"%s\" --pretty=format:\"%%ci\""%(who)
+  get_git_command = "git log --all --author=\"%s\" --pretty=format:\"%%ci\""%(who)
   analyse_data = run_sys_command(get_git_command)
   array_anlayse = analyse_data.split("\n")
+  i = 0
+  for enum_time_of_hour in time_of_hour:
+    enum_time_of_hour = 0
   for enum_analyse in array_anlayse:
-    print enum_analyse
-    print time.strptime(enum_analyse,"%Y-%m-%d %H:%M:%S +0800").tm_hour
+    time_hour = time.strptime(enum_analyse,"%Y-%m-%d %H:%M:%S +0800").tm_hour
+    time_start = 0
+    i = 0
+    while time_hour > time_start:
+      time_start = time_start+time_step
+      i = i + 1
+    time_of_hour[i-1] = time_of_hour[i-1]+1
+  i = 0
+  for enum_time_of_hour in time_of_hour:
+    print "sect %d-%d is "%(i*time_step,(i+1)*time_step),enum_time_of_hour
+    i = i + 1
 
